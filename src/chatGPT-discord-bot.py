e7962a00de7164ef6ec70aa5caa1a1b3dd72c006
Oriol Feliu <oriol.feliu9@gmail.com>
2023-04-29 13:00:03 +0200

    Code encapsulated in class

--- src/chatGPT-discord-bot.py
+++ src/chatGPT-discord-bot.py
@@ -3,84 +3,76 @@ import openai
 from api_keys import DISCORD_API_KEY, OPENAI_ORGANIZATION, OPENAI_API_KEY
 
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.messages = True
+class ChatGPTBot(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.messages = [{
+            "role": "system",
+            "content": "From now on, the messages you recieve will be from Discord users, so you are a Discord bot, act as such, don't give answers that are too long, but you are still chatGPT.",
+        }]
+        openai.api_key = OPENAI_API_KEY
 
-client = discord.Client(intents=intents)
+    async def generate_response(self):
+        try:
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=self.messages,
+                n=1
+            )
+            return completion.choices[0].message.content
 
-# OPENAI
-# openai.organization = OPENAI_ORGANIZATION
-openai.api_key = OPENAI_API_KEY
+        except openai.error.RateLimitError as e:
+            return f"Rate limit exceeded: {e}"
+        except openai.error.AuthenticationError as e:
+            return f"Invalid API key or organization: {e}"
+        except Exception as e:
+            return f"An error occurred: {e}"
 
-messages = [
-    {"role": "system", "content": "From now on, the messages you recieve will be from Discord users, so you are a Discord bot, act as such, don't give answers that are too long, but you are still chatGPT."}
-]
+    async def on_ready(self):
+        print("Logged in as {0.user}".format(self))
 
+    async def on_message(self, message):
+        try:
+            # Condition to check that the message is not sent by the bot
+            if message.author == self.user:
+                return
 
-# Define the function that sends a message to OpenAI API to generate a response
-async def generate_response(prompt):
-    try:
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            n=1, # number of chat completion choices to generate for each input message
-            # max_tokens = 100
-        )
+            print("--Message Content--")
+            print(message.content)
 
-        return completion.choices[0].message.content
+            if message.content.startswith("!c"):
+                await message.channel.send("Waiting for ChatGPT response...")
 
-    except openai.error.RateLimitError as e:
-        return f"Rate limit exceeded: {e}"
-    except openai.error.AuthenticationError as e:
-        return f"Invalid API key or organization: {e}"
-    except Exception as e:
-        return f"An error occurred: {e}"
+                prompt = message.content[3:]
 
+                self.messages.append({
+                    "role": "user",
+                    "content": prompt
+                })
 
-@client.event
-async def on_ready():
-    try:
-        print("Logged in as {0.user}".format(client))
-    except Exception as e:
-        print(f"Error during login process: {e}")
+                response = await self.generate_response()
 
+                self.messages.append({
+                    "role": "assistant",
+                    "content": response
+                })
 
-@client.event
-async def on_message(message):
-    global messages
-    try:
-        # Condition to check that the message is not sent by the bot
-        if message.author == client.user:
-            return
+                await message.channel.send(response)
 
-        print("--Message Content--")
-        print(message.content)
+            elif message.content.startswith("!new"):
+                print("--Conversation cleared--")
+                self.messages = [
+                    {
+                        "role": "system",
+                        "content": "From now on, the messages you recieve will be from Discord users, so you are a Discord bot, act as such, don't give answers that are too long",
+                    }
+                ]
 
-        if message.content.startswith("!c"):
-            await message.channel.send("Waiting for ChatGPT response...")
-
-            prompt = message.content[3:]
-            print(prompt)
-
-            messages.append({"role": "user", "content": prompt})
-
-            response = await generate_response(prompt)
-
-            messages.append({"role": "assistant", "content": response})
-
-            await message.channel.send(response)
-
-        elif message.content.startswith("!new"):
-            print("--Conversation cleared--")
-            messages = [
-                {"role": "system", "content": "From now on, the messages you recieve will be from Discord users, so you are a Discord bot, act as such, don't give answers that are too long"}
-            ]
-
-    except Exception as e:
-        await message.channel.send(
-            f"There was an error trying to generate the response:\n {e}"
-        )
+        except Exception as e:
+            await message.channel.send(
+                f"There was an error trying to generate the response:\n {e}"
+            )
 
 
+client = ChatGPTBot(intents=discord.Intents.default())
 client.run(DISCORD_API_KEY)
