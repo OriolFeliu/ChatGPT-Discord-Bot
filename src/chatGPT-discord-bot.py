59b3dca569fc5579d0d1d2f4ac762f49835b246d
Oriol Feliu <oriol.feliu9@gmail.com>
2023-04-29 12:21:42 +0200

    Functional bot, now it can remember past messages, error handling

--- src/chatGPT-discord-bot.py
+++ src/chatGPT-discord-bot.py
@@ -10,8 +10,12 @@ intents.messages = True
 client = discord.Client(intents=intents)
 
 # OPENAI
-openai.organization = OPENAI_ORGANIZATION
+# openai.organization = OPENAI_ORGANIZATION
 openai.api_key = OPENAI_API_KEY
+
+messages = [
+    {"role": "system", "content": "From now on, the messages you recieve will be from Discord users, so you are a Discord bot, act as such, don't give answers that are too long, but you are still chatGPT."}
+]
 
 
 # Define the function that sends a message to OpenAI API to generate a response
@@ -19,7 +23,8 @@ async def generate_response(prompt):
     try:
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": prompt}],
+            messages=messages,
+            n=1, # number of chat completion choices to generate for each input message
             # max_tokens = 100
         )
 
@@ -35,11 +40,15 @@ async def generate_response(prompt):
 
 @client.event
 async def on_ready():
-    print("Logged in as {0.user}".format(client))
+    try:
+        print("Logged in as {0.user}".format(client))
+    except Exception as e:
+        print(f"Error during login process: {e}")
 
 
 @client.event
 async def on_message(message):
+    global messages
     try:
         # Condition to check that the message is not sent by the bot
         if message.author == client.user:
@@ -53,9 +62,20 @@ async def on_message(message):
 
             prompt = message.content[3:]
             print(prompt)
+
+            messages.append({"role": "user", "content": prompt})
+
             response = await generate_response(prompt)
 
+            messages.append({"role": "assistant", "content": response})
+
             await message.channel.send(response)
+
+        elif message.content.startswith("!new"):
+            print("--Conversation cleared--")
+            messages = [
+                {"role": "system", "content": "From now on, the messages you recieve will be from Discord users, so you are a Discord bot, act as such, don't give answers that are too long"}
+            ]
 
     except Exception as e:
         await message.channel.send(
