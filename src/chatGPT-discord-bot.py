81c150d412f3569aeff01931afb8e07f5d04ebac
Oriol Feliu <oriol.feliu9@gmail.com>
2023-05-03 17:04:28 +0200

    Slash commands added

--- src/chatGPT-discord-bot.py
+++ src/chatGPT-discord-bot.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 import openai
 from api_keys import DISCORD_API_KEY, OPENAI_ORGANIZATION, OPENAI_API_KEY
 
@@ -11,6 +12,16 @@ class ChatGPTBot(discord.Client):
             "content": "From now on, the messages you recieve will be from Discord users, so you are a Discord bot, act as such, give answers that are as short as possible, but you are still chatGPT.",
         }]
         openai.api_key = OPENAI_API_KEY
+        self.synced = False
+
+    # TODO: Add a openAI api setter, so that every user can input it
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync()
+            self.synced = True
+        print("Logged in as {0.user}".format(self))
 
     async def generate_response(self):
         try:
@@ -27,9 +38,6 @@ class ChatGPTBot(discord.Client):
             return f"Invalid API key or organization: {e}"
         except Exception as e:
             return f"An error occurred: {e}"
-
-    async def on_ready(self):
-        print("Logged in as {0.user}".format(self))
 
     async def on_message(self, message):
         try:
@@ -70,12 +78,16 @@ class ChatGPTBot(discord.Client):
                     }
                 ]
 
+                message.channel.send("Conversation cleared.")
+
             elif message.content.startswith("!help"):
                 help_text = """
                 **Commands:**
                 `!c <prompt>` - Ask a question or start a conversation
+                `@ChatGPT` - Ask a question or start a conversation
                 `!new` - Clear the current conversation
                 `!help` - Show this help message
+                `/` - You can also use slash commands
                 """
                 await message.channel.send(help_text)
 
@@ -89,4 +101,53 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.messages = True
 client = ChatGPTBot(intents=intents)
+tree = app_commands.CommandTree(client)
+
+
+@tree.command(name="prompt", description="Ask a question or start a conversation")
+async def prompt_slash_command(interaction: discord.Interaction, prompt: str):
+    username = str(interaction.user)
+    channel = str(interaction.channel)
+    print(f" {username} said: '{prompt}' ({channel})")
+
+    client.messages.append({
+        "role": "user",
+        "content": prompt
+    })
+
+    response = await client.generate_response()
+
+    client.messages.append({
+        "role": "assistant",
+        "content": response
+    })
+
+    await interaction.response.send_message(response)
+
+
+@tree.command(name="clear", description="Clear the current conversation")
+async def clear_slash_command(interaction: discord.Interaction):
+    print("--Conversation cleared--")
+    client.messages = [
+        {
+            "role": "system",
+            "content": "From now on, the messages you recieve will be from Discord users, so you are a Discord bot, act as such, don't give answers that are too long",
+        }
+    ]
+
+    await interaction.channel.send("Conversation cleared.")
+
+
+@tree.command(name="help", description="Show help message")
+async def help_slash_command(interaction: discord.Interaction):
+    help_text = """
+                **Commands:**
+                `!c <prompt>` - Ask a question or start a conversation
+                `@ChatGPT` - Ask a question or start a conversation
+                `!new` - Clear the current conversation
+                `!help` - Show this help message
+                `/` - You can also use slash commands
+                """
+    await interaction.response.send_message(help_text)
+
 client.run(DISCORD_API_KEY)
